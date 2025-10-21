1712ccf9befb634c0cda52b2e63de7b803ee3d9e
felipe783 <felipebolt2007@Gmail.com>
2025-10-21 08:46:06 -0300

    Add files via upload

---
+++ 01.Hellow World.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) { //precisa ter esse main pra rodar so o arquivo
+        Scanner input=new Scanner(System.in); //Cria o scanf//
+        System.out.println("Hello World!"); //o ln pula linha//
+
+    }
+}
