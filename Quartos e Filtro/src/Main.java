d5a1453fc456e64be1d6c58f39a16623de4505ac
felipe783 <felipebolt2007@Gmail.com>
2025-10-23 09:57:19 -0300

    Enhance Main class with filters display
    Added imports and formatted output for filters.

--- Quartos e Filtro/src/Main.java
+++ Quartos e Filtro/src/Main.java
@@ -1,1 +1,16 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        ArrayList<Quartos> quartos = Quartos.sampleList();
+        Quartos.Exibir(quartos);
+        System.out.printf("%-10s","[Filtros]");
+        System.out.printf("\n%-10s","[Preco]--[0]");
+        System.out.printf("\n%-10s","[Qualidade]--[1]");
+        System.out.printf("\n%-10s","[Os dois]--[2]");
+        System.out.printf("\n--------------------------------------------------\n");
+    }
+
+}
 
