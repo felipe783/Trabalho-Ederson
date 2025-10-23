6c34ae5c5602d25caa939d67a68b8d2ede4e5ac9
felipe783 <felipebolt2007@Gmail.com>
2025-10-23 10:13:22 -0300

    Update Filtro.java

--- Quartos e Filtro/src/Filtro.java
+++ Quartos e Filtro/src/Filtro.java
@@ -4,11 +4,6 @@ import java.util.Scanner;
 public class Filtro {
     public static Scanner input = new Scanner(System.in);
 
-    public static void Exibir(String[] a){
-        for (String b : a) {
-            System.out.println(b);
-        }
-    }
     public static String[] Selecao_Filtro(String[] aux){
         String[] filtro_tipo = null;
         for (String tipo : aux) {
@@ -29,20 +24,8 @@ public class Filtro {
         }
         return filtro_tipo;
     }
+    public boolean matches(String[] filtro){
 
-    public static void main(String[] args){
-        ArrayList<Quartos> quartos = new ArrayList<>();
-        System.out.printf("\n-----------------------------------");
-        System.out.printf("\nFiltros");
-        System.out.printf("\n[Preco]--[0]");
-        System.out.printf("\n[Qualidade]--[1]");
-        System.out.printf("\n[Os dois]--[2]");
-        System.out.printf("\n-----------------------------------\n");
-
-        // Input usuario
-        System.out.println("Quais filtro voce deseja adicionar:");
-        String[] Filtro_Desejado = input.nextLine().toLowerCase().split(" ");
-        String[] resultado = Selecao_Filtro(Filtro_Desejado);
-        Exibir(resultado);
+        return false;
     }
 }
