79a47f1f25ed20cba71a49c0becb6fc0c06f32e6
gsdbrito <gsdbrito@minha.fag.edu.br>
2025-10-23 10:27:40 -0300

    Create Quarto.java

---
+++ Quarto.java
@@ -0,0 +1,14 @@
+public class QuartoID {
+     int ID;
+     String tipo;
+     double preco;
+     int entrada;
+     int saida;
+    public QuartoID(int ID, String tipo, double preco,int entrada,int saida) {
+        this.ID = ID;
+        this.tipo = tipo;
+        this.preco = preco;
+        this.entrada = entrada;
+        this.saida = saida;
+    }
+}
