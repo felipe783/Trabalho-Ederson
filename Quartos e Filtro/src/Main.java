b7076256627651069aa3a769fda3c64c364a30c7
felipe783 <felipebolt2007@Gmail.com>
2025-10-23 10:13:32 -0300

    Update Main.java

--- Quartos e Filtro/src/Main.java
+++ Quartos e Filtro/src/Main.java
@@ -2,7 +2,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    public static Scanner input = new Scanner(System.in);
     public static void main(String[] args){
+        //Cria o objeto de quartos pra exibir
         ArrayList<Quartos> quartos = Quartos.sampleList();
         Quartos.Exibir(quartos);
         System.out.printf("%-10s","[Filtros]");
@@ -10,7 +12,21 @@ public class Main {
         System.out.printf("\n%-10s","[Qualidade]--[1]");
         System.out.printf("\n%-10s","[Os dois]--[2]");
         System.out.printf("\n--------------------------------------------------\n");
+
+        /**Selecionar o tipo do filtro**/
+        System.out.println("Quais filtro voce deseja adicionar:");
+        String[] Filtro_Desejado = input.nextLine().toLowerCase().split(" ");
+
+        /**Selecionar quais seram os filtros**/
+        String[] resultado = Filtro.Selecao_Filtro(Filtro_Desejado);
+
+        /**Aplicar o filtro**/
+
+        System.out.printf("Quartos disponiveis conforme o filtro:");
+        for(Quartos q : quartos){
+            /*if(q.Comparar(filtro_tipo)){
+                System.out.print(q);
+            }*/
+        }
     }
-
 }
-
