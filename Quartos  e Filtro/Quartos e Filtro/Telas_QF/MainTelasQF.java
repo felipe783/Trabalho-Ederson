6af16f330bf60f3ac325ef8680dc9328498b5d1c
Thiago <thyagodelimamachado@gmail.com>
2025-11-13 00:08:43 -0300

    Ajuste telas/reserva 12/11

--- Quartos  e Filtro/Quartos e Filtro/Telas_QF/MainTelasQF.java
+++ Quartos  e Filtro/Quartos e Filtro/Telas_QF/MainTelasQF.java
@@ -11,19 +11,22 @@ public class MainTelasQF extends JFrame {
         setSize(400,300);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        setLayout(new GridLayout(3,1,10,10));
+        setLayout(new GridLayout(4,1,10,10));
 
         JButton btFiltro = new JButton("Filtro");
         JButton btQuarto = new JButton("Quartos");
+        JButton btReservar = new JButton("Reservas");
         JButton btSair = new JButton("Sair");
 
         add(btFiltro);
         add(btQuarto);
+        add(btReservar);
         add(btSair);
 
-        btSair.addActionListener(e -> dispose());
-        btFiltro.addActionListener(e->new TelaFiltro().setVisible(true));
-        btQuarto.addActionListener(e->new TelaQuarto().setVisible(true));
+        btSair.addActionListener(_ -> dispose());
+        btFiltro.addActionListener(_->new TelaFiltro().setVisible(true));
+        btQuarto.addActionListener(_->new TelaQuarto().setVisible(true));
+        btReservar.addActionListener(_->new TelaReserva().setVisible(true));
     }
 
     public static void main(String[] args){
